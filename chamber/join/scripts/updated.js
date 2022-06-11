6d8ffdef438f819b9eb89b7e6a3b696899fcf75f
mictlantecutli <moisesparra82@hotmail.com>
2022-06-11 15:49:10 -0500

    join page

---
+++ chamber/join/scripts/updated.js
@@ -0,0 +1,59 @@
+//This code is to get the year in the footer
+let current_date = new Date();
+
+document.querySelector(".year").innerHTML = `${current_date.getFullYear()}`
+
+//This code is for the las modification of the document
+document.querySelector(".date_updated").innerHTML = `last updated: ${document.lastModified}`
+
+//This code is for the date in the header
+const datefield = document.querySelector("#date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "full"
+}).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+//This code for the hamburger menu
+const hambutton = document.querySelector('#ham');
+const mainnav = document.querySelector('nav')
+
+hambutton.addEventListener("click", function(){
+  
+  mainnav.classList.toggle("responsive")
+});
+
+//const banner = document.querySelector(".two");
+//banner.style.display = "block";
+//banner.style.backgroundImage = "url('https://mictlantecutli.github.io/wdd230/chamber/images/plantita_business.jpg')";
+
+let d = new Date().getDay();
+const pop = document.querySelector(".pop");
+if (d === 1 || d === 2){
+  pop.style.display = "block"
+
+}
+
+const close = document.querySelector(".close");
+close.addEventListener("click", () => {
+  pop.style.display = "none";
+  
+})
+
+
+/*************This is the code fot the temperature*******************/
+let temperatureCelsius = parseFloat(document.getElementById("temp").innerText);
+let speedWind = parseFloat(document.getElementById("speed").innerText);
+
+
+let temperatureFar = (temperatureCelsius * 1.8) + 32;
+let speedMph = speedWind * 0.62137
+
+if (temperatureFar < 51 && speedMph > 3) {
+    let wChill = 35.74 + 0.6215 * temperatureFar - 35.75 * Math.pow(speedMph, .16) + .4275 * temperatureFar * Math.pow(speedMph, .16);
+    document.getElementById("chill").innerText = parseInt(wChill);
+}else{
+    document.getElementById("chill").innerText = "N/A";
+}
+/****************here finish the code of temperature******************/
+
---
+++ chamber/thankyou_page/scripts/hidden.js
@@ -0,0 +1,9 @@
+/*Here I get the date to send it to the hidden input*/
+var date = new Date();
+
+/*Then I get the ID where the date shall stored*/
+document.getElementById("date").value = date.toDateString();
+
+/*I get the hour and minutes to send in the hidden input*/
+
+
