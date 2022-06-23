e73554da4b266a6e261491b83c3b8ae9c44cb092
mictlantecutli <moisesparra82@hotmail.com>
2022-06-23 16:58:41 -0500

    weather api

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,34 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=4b295a19abeeb2912f33ca4087040e34'
+
+
+async function apiFetch(apiUrl) {
+
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+ 
+}
+
+apiFetch(url);
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.innerHTML = desc
+
+}
